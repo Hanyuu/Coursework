5bccadcddd3aa79c6b20990a3c7df03177106242
Hanyuu <Hanyuu@SVladimir>
2012-10-16 13:22:44 +0600

    Init

---
+++ src/coursework/Coursework.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursework;
+
+/**
+ *
+ * @author Hanyuu
+ */
+public class Coursework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
